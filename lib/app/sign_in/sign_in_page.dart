ccd333c2d0add65ac76b61b36c62fd980f0c9094
Mohamad Afif Zuhair <afifzuhair08@gmail.com>
2020-02-27 13:31:09 +0800

    add sign in page with single method extracted

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -3,6 +3,46 @@ import 'package:flutter/material.dart';
 class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+        centerTitle: true,
+        elevation: 5.0,
+      ),
+      body: buildContent(),
+    );
+  }
+
+  //container for -> body: buildContent <-
+  Widget buildContent() {
+    return Container(
+      color: Colors.yellow,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            color: Colors.orange,
+            child: SizedBox(
+              //sizedbox with orange color
+              height: 50.0,
+            ),
+          ),
+          Container(
+            color: Colors.red,
+            child: SizedBox(
+              //sizedbox with orange color
+              height: 50.0,
+            ),
+          ),
+          Container(
+            color: Colors.purple,
+            child: SizedBox(
+              //sizedbox with orange color
+              height: 50.0,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
