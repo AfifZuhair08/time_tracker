e8c74dfaaafbe1f7143241d0ed59f4de528b6655
Mohamad Afif Zuhair <afifzuhair08@gmail.com>
2020-02-27 16:02:45 +0800

    added raised button for sign in and folder to store common widget

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -10,39 +10,51 @@ class SignInPage extends StatelessWidget {
         elevation: 5.0,
       ),
       body: _buildContent(),
+      backgroundColor: Colors.grey[200],
     );
   }
 
   //container for -> body: _buildContent <-
   Widget _buildContent() {
-    return Container(
-      color: Colors.yellow,
+    return Padding(
+      padding: EdgeInsets.all(16.0),//set all edge with same one value
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Container(
-            color: Colors.orange,
-            child: SizedBox(
-              //sizedbox with orange color
-              height: 50.0,
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
             ),
           ),
-          Container(
-            color: Colors.red,
-            child: SizedBox(
-              //sizedbox with orange color
-              height: 50.0,
+          SizedBox(height: 8.0),
+          RaisedButton(
+            child: Text(
+              'Sign in with Google',
+              style: TextStyle(
+                color: Colors.black45, 
+                fontSize: 15.0
+              ),
             ),
-          ),
-          Container(
-            color: Colors.purple,
-            child: SizedBox(
-              //sizedbox with orange color
-              height: 50.0,
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(16.0),
+              )
             ),
+            onPressed: (){
+              print('button Google pressed');
+            }
           )
         ],
       ),
     );
   }
+
+
+
+
 }
